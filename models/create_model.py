dad80a64099f6491fa767c581061c8fbab6a36ec
AkashVermaIN <119874166+AkashVermaIN@users.noreply.github.com>
2024-11-27 13:02:51 +0530

    Add files via upload

---
+++ models/create_model.py
@@ -0,0 +1,50 @@
+def create_model(img_size, n_classes, args):
+    if args.model == 'vit':
+        from .vit import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)
+    if args.model == 'vitr4':
+        from .vitr4 import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)
+    if args.model == 'vitr8':
+        from .vitr8 import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)
+    if args.model == 'vitr12':
+        from .vitr12 import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)
+    if args.model == 'vitr16':
+        from .vitr16 import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)
+    if args.model == 'vitmean':
+        from .vitmean import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)    
+    if args.model == 'vitcls':
+        from .vitcls import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)  
+    if args.model == 'vitsata':
+        from .vitsata import ViT
+        patch_size = 4 if img_size == 32 else 8
+        model = ViT(img_size=img_size, patch_size = patch_size, num_classes=n_classes, dim=192, 
+                    mlp_dim_ratio=2, depth=9, heads=12, dim_head=192//12,
+                    stochastic_depth=0.1)                        
+    return model
